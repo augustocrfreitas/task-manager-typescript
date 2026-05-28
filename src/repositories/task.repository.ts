2da43cadd282e37a50e2c40b58ea6fc8ed480050
augustocrfreitas18 <augusto180402@gmail.com>
2026-05-28 19:15:34 -0300

    finalizando rotas Task

--- src/repositories/task.repository.ts
+++ src/repositories/task.repository.ts
@@ -5,7 +5,21 @@ export default class TaskRepository {
     static async getTasks(userId: string): Promise<Task[]> {
         const result = await pool.query(
             `
-            SELECT * FROM "Task" WHERE "createdBy" = $1
+            SELECT 
+            t.id,
+            t.title,
+            t.description,
+            t.completed, 
+            t."createdAt",
+            t."updatedAt",
+            tm.name AS "teamName",
+            c.name AS "createdByName",
+            a.name AS "assigneeName"
+            FROM "Task" t
+            LEFT JOIN "User" c ON t."createdBy" = c.id
+            LEFT JOIN "User" a ON t."assigneeTo" = a.id
+            LEFT JOIN "Team" tm ON t."teamId" = tm.id
+            WHERE t."createdBy" = $1    
             `,
             [userId],
         );
@@ -15,7 +29,21 @@ export default class TaskRepository {
     static async getTaskById(id: string): Promise<Task> {
         const result = await pool.query(
             `
-            SELECT * FROM "Task" WHERE id = $1  
+            SELECT 
+            t.id,
+            t.title,
+            t.description,
+            t.completed, 
+            t."createdAt",
+            t."updatedAt",
+            tm.name AS "teamName",
+            c.name AS "createdByName",
+            a.name AS "assigneeName"
+            FROM "Task" t
+            LEFT JOIN "User" c ON t."createdBy" = c.id
+            LEFT JOIN "User" a ON t."assigneeTo" = a.id
+            LEFT JOIN "Team" tm ON t."teamId" = tm.id 
+            WHERE t.id = $1  
             `,
             [id],
         );
@@ -23,11 +51,28 @@ export default class TaskRepository {
     }
 
     static async createTask(data: CreateTaskInput): Promise<Task> {
+        console.log(data);
         const result = await pool.query(
             `
-            INSERT INTO "Task" (title, description, teamId, assigneeTo, createdBy)
-            VALUES ($1, $2, $3, $4, $5)
-            RETURNING *
+           WITH inserted_task AS (
+                INSERT INTO "Task" (title, description, "teamId", "assigneeTo", "createdBy")
+                VALUES ($1, $2, $3, $4, $5)
+                RETURNING *
+            )
+            SELECT 
+                t.id,
+                t.title,
+                t.description,
+                t.completed, 
+                t."createdAt",
+                t."updatedAt",
+                tm.name AS "teamName",
+                c.name AS "createdByName",
+                a.name AS "assigneeName"
+                FROM inserted_task t
+                LEFT JOIN "User" c ON t."createdBy" = c.id
+                LEFT JOIN "User" a ON t."assigneeTo" = a.id
+                LEFT JOIN "Team" tm ON t."teamId" = tm.id
             `,
             [data.title, data.description, data.teamId, data.assigneeTo, data.createdBy],
         );
--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 
 export const taskRouter = Router();
 
-taskRouter.get('/:id', TaskController.getTasks);
-taskRouter.get('/id/:id', TaskController.getTaskById);
+taskRouter.get('/user/:userId', TaskController.getTasks);
+taskRouter.get('/:id', TaskController.getTaskById);
 
 taskRouter.post('/', TaskController.createTask);
