d6c4556cdeb2462923f9d9df39ad44e2f69d313b
augustocrfreitas18 <augusto180402@gmail.com>
2026-05-29 07:53:56 -0300

    instalando cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import 'dotenv/config';
+import cors from 'cors';
 import { userRouter } from './routes/user.route';
 import { teamRouter } from './routes/team.route';
 import { taskRouter } from './routes/task.route';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 3333;
 
